64607dff609af82216a04c03b4c7be30a5bee351
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-09 21:05:22 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,78 @@
+//Array of objects
+const quiz = [
+    {
+        q:"An American woman ___ a newspaper when she ___ a photo of her husband.",
+        options:["was reading / saw", "had read / saw", "read / had seen"],
+        answer: 0
+    },
+    {
+     q:"He was working in the US navy when he ___ of a crime.",
+        options:["had been accused", "was accused", "accused"],
+        answer: 1   
+    },
+    {
+        q:"He ___ in the UK when he ___ .",
+        options:["had lived / got married", "lived / had got married", "was living / got married"],
+        answer: 2
+    },
+    {
+        q:"She ___ the children that their father ___ to prison.",
+        options:["didn't tell / had gone", "wasn't telling / went", "hadn't told / went"],
+        answer: 0
+    },
+    {
+        q:"The judge ___ the teenager until she ___ to the evedence.",
+        options:["wasn't sentenced / listened", "didn't sentence / had listened", "hadn't sentenced / was listened"],
+        answer: 1
+    },
+    {
+        q:"After they ___ breakfast, they went to work.",
+        options:["were eating", "ate", "had eaten"],
+        answer: 2
+    },
+    {
+        q:"When they have an accident, they ___ at 170 kph.",
+        options:["were driving", "had driven", "drove"],
+        answer: 0
+    },
+    {
+        q:"When he crashed the car, an ambulance ___ and ___  him to hospital.",
+        options:["had arrived / had taken", "arrived / took", "arrived / had taken"],
+        answer: 1
+    },
+    {
+        q:"When the judge returned to the courtroom, she ___ her decision.",
+        options:["made", "was making", "had made"],
+        answer: 2
+    },
+    {
+        q:"When he came out of the prison gate, he ___ his wife and then they ___ home.",
+        options:["had kissed / went", "kissed / went", "kissed / had gone"],
+        answer: 0
+    },
+    {
+        q:"Yesterday morning when I got up and looked out of the window, the ground was very wet. It ___ .",
+        options:["was raining", "had been raining", "had rained"],
+        answer: 1
+    },
+    {
+     q:"The football match had to be stopped. They ___ for half an hour when there was a terrible storm.",
+        options:["played", "was playing", "had been playing"],
+        answer: 2   
+    },
+    {
+        q:"The woman was a complete stranger to me. I ___ never ___ her before.",
+        options:["had / seen", "hadn't / seen", "was / seen"],
+        answer: 0
+    },
+    {
+        q:"The house was very quiet when I got hom. Everybody ___ to bed.",
+        options:["went", "had gone", "was going"],
+        answer: 1
+    },
+    {
+        q:"The car was very dirty. I ___ it for weeks.",
+        options:["didn't clean", "wasn't cleaned", "hadn't cleaned"],
+        answer: 2
+    }
+]
